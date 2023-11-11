aa4217c5745661b3a40e00fbb9a2a5f47a189a22
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-11-11 13:45:02 -0300

    configuration project and shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hollo Admin Dashboard</p>
+      <Button >Click here</Button>
+    </div>
+  )
+}
