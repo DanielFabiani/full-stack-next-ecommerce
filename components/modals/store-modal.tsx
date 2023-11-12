3958b085b7a954578a8f6db78b0d184165ca7d71
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-11-12 15:23:41 -0300

    route api and form create new store done

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -36,7 +36,7 @@ export const StoreModal = () => {
       setLoading(true);
 
       const response = await axios.post('/api/stores', values);
-      //console.log(response.data);
+
       toast.success('Store created.');
 
     } catch (error) {
