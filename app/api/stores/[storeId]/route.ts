05ef0764214351e95dcb55ebd3b2a5432a17a725
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-11-13 17:45:33 -0300

    modal for delete store and change name in dasboard done

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function PATCH (
   req: Request,
-  res: Response,
   { params }: {params: { storeId: string } }
   ) {
     try {
@@ -42,4 +41,36 @@ export async function PATCH (
       console.log('[STORES_PATCH]', error);
       return new NextResponse('Internal error', { status: 500 });
     }
-  }
+  };
+
+export async function DELETE (
+  req: Request,
+  { params }: {params: { storeId: string } }
+  ) {
+    try {
+      const { userId } = auth();
+
+      if (!userId) {
+        return new NextResponse("Unauthorized", { status: 401 });
+      }
+
+      if(!params.storeId) {
+        return new NextResponse("Store is required", { status: 400 });
+      }
+
+      const store = await prismadb.store.deleteMany({ 
+        where: {
+          id: params.storeId,
+          userId
+        }
+      })
+
+      return NextResponse.json(store);
+
+    } catch (error) {
+      console.log('[STORES_DELETE]', error);
+      return new NextResponse('Internal error', { status: 500 });
+    }
+  };
+
+  
